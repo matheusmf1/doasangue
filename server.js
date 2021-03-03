115a02e61367f8f3ef5fb760a8cb46d96d5bb984
Matheus Mandotti <mathew.mfranco@gmail.com>
2021-03-03 15:59:36 -0300

    Update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const Pool = require('pg').Pool
 const db = new Pool({
     user: 'admin',
     password: 'admin1234',
-    host: 'rdsdemo.czs5dtq7qb7g.us-east-1.rds.amazonaws.com',
+    host: 'myrdsdemo.czs5dtq7qb7g.us-east-1.rds.amazonaws.com',
     port: 3306,
     database: 'doasangue'
 })
@@ -32,42 +32,36 @@ nunjucks.configure("./", {
 })
 
 server.get( '/create', ( req, res ) => {
+    
+    db.query( `CREATE TABLE IF NOT EXISTS donors(
+        ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(45),
+        email VARCHAR(60),
+        blood VARCHAR(45)
+    )`,
+    (err, result) => {
 
-    // db.query( `CREATE TABLE IF NOT EXISTS donors(
-    //     ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    //     name VARCHAR(45),
-    //     email VARCHAR(60),
-    //     blood VARCHAR(45)
-    // )`,
-    // (err, result) => {
-    //     if (err) return res.send("Erro de banco de dados parte 1");
-    //     console.log('result: ' + result)
+        if (err) return res.send("Erro de banco de dados parte 1");
+        console.log('result: ' + result)
 
-    //     logger.warn('Banco de dados error'); 
-    //     const donors = result.rows;
-    //     return res.render("index.html")
-    // })
-
-    db.query( "SHOW TABLES;", ( err, result ) => {
-        console.log( 'err: ' + err )
-        console.log( 'result: ' + result )
-
-        return res.render("index.html", { })
-    } )
+        logger.warn('Banco de dados error'); 
+        // const donors = result.rows;
+        return res.render("index.html", {})
+    })
 })
 
 server.get("/", function(req, res) {
-    db.query("SELECT * FROM donors", function(err, result){
-        if (err) return res.send("Erro de banco de dados parte 1");
+    // db.query("SELECT * FROM donors", function(err, result){
+    //     if (err) return res.send("Erro de banco de dados parte 1");
 
-        logger.warn('Banco de dados error'); 
-        const donors = result.rows;
-        return res.render("index.html", { donors })
-    })
+    //     logger.warn('Banco de dados error'); 
+    //     const donors = result.rows;
+    //     return res.render("index.html", { donors })
+    // })
 
-    donors = []
+    // donors = []
 
-    // res.render("index.html", { })
+    res.render("index.html", { })
     
 })
 
